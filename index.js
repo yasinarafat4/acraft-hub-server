b93e82d4ce6718270d8c0aa73dc6f0267e88047a
Md. Yasin Arafat <mdyasinarafathasib@gmail.com>
2023-06-11 12:24:14 +0600

    Creating make admin API done

--- index.js
+++ index.js
@@ -38,6 +38,11 @@ async function run() {
       .collection("instructors");
 
     // Users related API's
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       console.log(user);
@@ -48,6 +53,19 @@ async function run() {
         return res.send({ message: "User already exist" });
       }
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // Make admin API
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
 
